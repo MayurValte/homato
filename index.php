86181f2741cb6990871b7b7e5a6b9be055e66ca9
Mayur Valte <mayurvalte1999@gmail.com>
2020-12-26 12:47:34 +0530

    homato

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>prac</title>
+	            <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+</head>
+<body>
+	<button onclick="popup()" id=btn>Submit</button>
+	<script>
+		function popup(){
+		swal("Good job!", "Successfully Submited!", "success");
+
+		}
+	</script>
+
+</body>
+</html>
